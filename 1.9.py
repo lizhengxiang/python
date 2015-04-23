5da5c0455ecacd36896fe485b2a4353e0c4feb83
lizhengxiang <1065482100@qq.com>
2015-04-23 23:37:31 +0800

    The module

---
+++ 1.9.py
@@ -0,0 +1,12 @@
+import math
+import cmath
+print math.floor(32.9)
+
+from math import sqrt
+print sqrt(9)
+
+foo = math.sqrt
+#print foo(-4)   # math domian error
+
+c_math = cmath.sqrt
+print c_math(-4)
