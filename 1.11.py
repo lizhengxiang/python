c70b5d63ea7a4c7b8dc96c8d00e0a97074782895
lizhengxiang <1065482100@qq.com>
2015-04-24 23:55:02 +0800

    String

--- 1.11.py
+++ 1.11.py
@@ -7,3 +7,11 @@ print str("lizhengxiang")
 print str(1000l)
 temp = 45
 print "hello world " + repr(temp)
+name = input("what your name ? ")
+print "hello" + name + '!'
+name_ = raw_input("what you name ? ")
+print "hello " + name_ + "!"
+print """ hello world.\
+lizhengxiang.\
+!"""
+
