9cd2bca4c81d7de9fb8bfccc25a31d56d71c3a0e
lizhengxiang <1065482100@qq.com>
2015-04-25 17:50:50 +0800

    python

---
+++ 2.2.5.py
@@ -0,0 +1,11 @@
+subject = '$$$ lzx ***'
+print '$$$' in subject
+print '***' in subject
+print subject in '$$$'
+permissions = 'rw'
+x = raw_input()
+print x in permissions
+Users = ['lizhangxiang', 'tom', 'lzx']
+y = raw_input('User name: ')
+print y in Users
+
