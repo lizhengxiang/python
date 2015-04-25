88e7c25f277afc6b5b66410ca7f901902fb901b7
lizhengxiang <1065482100@qq.com>
2015-04-25 17:49:38 +0800

    python

---
+++ 2.2.2.py
@@ -0,0 +1,24 @@
+number = [1,2,3,4,5,6,7,8,9,0]
+print number[2:5]
+print number[2:17]
+#print number[-1, -3]
+print number[:]
+print number[2:]
+print number[:5]
+print number[-3:-1]
+print number[2:-2]
+print number[0: :2]
+print number[::2]
+print number[::-1]
+print number[::-2]
+print '\n'
+print [1,1,1] + [2, 3, 4]
+print 'li ' + 'zhang ' + 'xiang'
+print 'lizhang' + str([123])
+print '\n'
+print 'lizhengxiang' * 5
+print ['lizhengxing'] * 5
+print [23] * 5
+print [23] + [5]
+print [0] * 10
+print [None] * 10
