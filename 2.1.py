86958229d3f444e02d0a411a7e2ccc1ca75e5d1f
lizhengxiang <1065482100@qq.com>
2015-04-25 17:42:56 +0800

    python

---
+++ 2.1.py
@@ -0,0 +1,12 @@
+xiang = ["lizhengxiang", 24]
+qian = ["lizhangqian", 25]
+
+database = [xiang, qian]
+print database
+
+getting = "xiang"
+print getting[2]
+print getting[-3]
+print "xiang"[2]
+print raw_input("age: ")[0]
+print 17 * ['th']
