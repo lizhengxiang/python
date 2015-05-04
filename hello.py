49b278c2f2f044b880e216e03a8917a1f1f9dce8
lizhengxiang <1065482100@qq.com>
2015-05-04 20:45:26 +0800

    Flask

--- hello.py
+++ hello.py
@@ -1,20 +1,26 @@
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
-from flask.exe.wtf import From
-from wtforms import StringFiled, SubmitFiled
-from wtforms.validators import Require
+#from flask.exe.wtf import From
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 #from flask.exe.moment import Moment
 from datetime import datetime
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
 #moment = Moment(app)
-class NameFrom(From):
-	name = StringFiled('what you name?', validators = [Require()])
-	submit = SubmitFiled('Submit')
-@app.route('/')
+class NameFrom(Form):
+	name = StringField('what you name?', validators = [Required()])
+	submit = SubmitField('Submit')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+	name = None
+	form = NameFrom()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', form = form, name = name)
 @app.route('/user/<name>')
 def user(name):
 	return render_template('user.html', name=name)
