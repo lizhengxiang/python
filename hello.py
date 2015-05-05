a9933026154851338ee6facf8d6ef99596263990
lizhengxiang <1065482100@qq.com>
2015-05-05 16:48:45 +0800

    flask web

--- hello.py
+++ hello.py
@@ -1,18 +1,29 @@
 from flask import Flask, render_template, session, url_for, redirect, flash
 from flask.ext.bootstrap import Bootstrap
-#from flask.exe.wtf import From
+#from flask.ext.sqlalchemy import SQLAlchemy 
+from flask.ext.sqlalchemy import SQLAlchemy
+#from flask.ext.sqlalchemy import SQLAlchemy 
 from flask.ext.wtf import Form
+from flask.ext.mail import Mail
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 #from flask.exe.moment import Moment
 from datetime import datetime
+#from flaskext.sqlalchemy import SQLAlchemy
+import os
 app = Flask(__name__)
+mail = Mail(app)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
 #moment = Moment(app)
 class NameFrom(Form):
 	name = StringField('what you name?', validators = [Required()])
 	submit = SubmitField('Submit')
+app.config['MAIL_SEVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	name = None
