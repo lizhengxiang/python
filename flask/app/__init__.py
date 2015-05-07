f90140a938c4863638ac2d3e8486125ec8572495
lizhengxiang <1065482100@qq.com>
2015-05-07 23:48:48 +0800

    flask web

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,12 +1,6 @@
-from flask import Flask, render_template
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.mail import Mail
-from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
-from config import config
+#from config import config
 
-bootstrap = Bootstrap()
-mail = Mail()
 db = SQLAlchemy()
 
 def create_app(config_name):
