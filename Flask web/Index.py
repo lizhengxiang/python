7e371e239f7008c3b00560f07c244ce076a78fe1
lizhengxiang <1065482100@qq.com>
2015-05-09 14:21:58 +0800

    Flask web

--- Flask web/Index.py
+++ Flask web/Index.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session, url_for, redirect, flash
 from flask.ext.bootstrap import Bootstrap
-from flask.ext.sqlalchemy import SQLAlchemy 
+#from flask.ext.sqlalchemy import SQLAlchemy 
 from flask.ext.sqlalchemy import SQLAlchemy
 #from flask.ext.sqlalchemy import SQLAlchemy 
 from flask.ext.wtf import Form
@@ -53,8 +53,6 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-
-#form = NameFrom(Form)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
--- Flask web/main.py
+++ Flask web/main.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
-
+from flask.ext.login import UserMixin
 app = Flask(__name__)
-
+from NameForm import NameForm
 #define base moldboard
 bootstrpa = Bootstrap(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username = form.name.data).first()
+		if user is None:
+			user = User(username = form.name.data)
+			db.session.add(user)
+			session['known'] = False
+		else:
+			session['known'] = True
+		session['name'] = form.name.data
+		form.name.data = ''
+		old_name = session.get('name')
+  		if old_name is not None and old_name != form.name.data:
+ 			flash('Looks link you have changed you name!')
+		return redirect(url_for('index'))
+	return render_template('index.html', form = form, name = session.get('name'), known = session.get('known', False))
 
 @app.route('/user/<name>')
 def user(name):
