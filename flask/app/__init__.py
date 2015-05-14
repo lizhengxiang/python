471495cf6c7ef06fb6a2b6624a3b3dd9468d2a26
lizhengxiang <1065482100@qq.com>
2015-05-14 09:22:38 +0800

    flask web find bug

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask.ext.bootstrap import Bootstrap		
 from flask.ext.mail import Mail		
 from flask.ext.moment import Moment
-from flask.ext.login import LoginManager
+#from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
 from config import config
 
@@ -20,9 +20,9 @@ mail = Mail()
 db = SQLAlchemy( )
 moment = Moment()
 
-login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'auth.login'
+#login_manager = LoginManager()
+#login_manager.session_protection = 'strong'
+#login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
 	app = Flask(__name__)
@@ -44,7 +44,7 @@ def create_app(config_name):
 	#db.app = app
 	#db.create_all()
 	#db = upgrade()
-	login_manager.init_app(app)
+	#login_manager.init_app(app)
 
 	from .main import main as main_blueprint
 	app.register_blueprint(main_blueprint)
