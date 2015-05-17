f801953c686a123573e3115ae8b1b37385f45541
lizhengxiang <1065482100@qq.com>
2015-05-17 12:20:10 +0800

    flask web

--- flasky/app/auth/views.py
+++ flasky/app/auth/views.py
@@ -48,17 +48,31 @@ def confirm(token):
 		return redirect(url_for('main.index'))
 	if current_user.confirm(token):
 		flash('You have confirmed you account. Thanks!')
-	else
+	else:
 		flash('The confirmed link is invalid or has expired.')
 	return redirect(url_for('mian.index'))
 
+#@auth.before_app_request
+#def before_request():
+#	if current_user.is_authenticated()\
+#			and not current_user.confirmed\
+#			and request.endpoint[:5] != 'auth.':\
+#			and request.endpoint != 'static':
+#		return redirect(url_for('auth.unconfirmed'))
+
 @auth.before_app_request
 def before_request():
-	if current_user.is_authenticated()\
-			and not current_user.confirmed\
-			and request.endpoint[:5] != 'auth.'
-			and request.endpoint != 'static':
-		return redirect(url_for('auth.unconfirmed'))
+    if current_user.is_authenticated():
+        #current_user.ping()
+        if not current_user.confirmed \
+                and request.endpoint[:5] != 'auth.' \
+                and request.endpoint != 'static':
+            return redirect(url_for('auth.unconfirmed'))
+
+
+
+
+
 @auth.route('/unconfirmed')
 def unconfirmed():
 	if current_user.is_anonymous() or current_user.confirmed:
@@ -66,11 +80,11 @@ def unconfirmed():
 	return render_template('auth/unconfirmed.html')
 
 @auth.route('/confirm')
-@login_requried
+@login_required
 def resend_confirmation():
-	token = user.generate_confirmation_token()
-	send_email(user.email, 'Confirm Your Account',
-		'auth/email/confirm', user=user, token=token)
+	token = current_user.generate_confirmation_token()
+	send_email(current_user.email, 'Confirm Your Account',
+		'auth/email/confirm', user=current_user, token=token)
 	flash('A confirmation email has been sent to you by email.')
 	return redirect(url_for('main.index'))
 
