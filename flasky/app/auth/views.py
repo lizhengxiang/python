9b0b2dd9fc3f81c92c27894ceea5ef4c1847c2c6
lizhengxiang <1065482100@qq.com>
2015-05-17 09:34:26 +0800

    flask web email

--- flasky/app/auth/views.py
+++ flasky/app/auth/views.py
@@ -53,7 +53,17 @@ def confirm(token):
 	return redirect(url_for('mian.index'))
 
 @auth.before_app_request
-
+def before_request():
+	if current_user.is_authenticated()\
+			and not current_user.confirmed\
+			and request.endpoint[:5] != 'auth.'
+			and request.endpoint != 'static':
+		return redirect(url_for('auth.unconfirmed'))
+@auth.route('/unconfirmed')
+def unconfirmed():
+	if current_user.is_anonymous() or current_user.confirmed:
+		return redirect(url_for('main.index'))
+	return render_template('auth/unconfirmed.html')
 
 
 
