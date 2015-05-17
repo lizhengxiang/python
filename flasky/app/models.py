72c4b2447775b55b6199d2dd16c73814b5b304b3
lizhengxiang <1065482100@qq.com>
2015-05-17 14:11:48 +0800

    database role

--- flasky/app/models.py
+++ flasky/app/models.py
@@ -5,56 +5,86 @@ from flask.ext.login import UserMixin
 from . import db, login_manager
 
 
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')
+class permission:
+	FOLLOW = 0X01
+	COMMENT = 0X02
+	WRITE_ARTICLES = 0X04
+	MODERATE_COMMENTS = 0X08
+	ADMINISTER = 0X80
 
-    def __repr__(self):
-        return '<Role %r>' % self.name
+class Role(db.Model):	
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+	default = db.Column(db.Boolean, default = False, index = True)
+	permissions = db.Column(db.Integer)
+	users = db.relationship('User', backref='role', lazy='dynamic')
+
+	@staticmethod
+	def insert_roles():
+		roles = {
+			'User':(permission.FOLLOW |
+				permission.COMMENT |
+				permission.WRITE_ARTICLES, True),
+			'Moderator':(permission.FOLLOW |
+				permission.COMMENT |
+				permission.WRITE_ARTICLES |
+				permission.MODERATE_COMMENTS, False),
+			'Administration':(0xff, False)
+		}
+		for r in roles:
+			role = Role.query.filter_by(name=r).first()
+			if role is None:
+				role = Role(name=r)
+			role.permissions = roles[r][0]
+			role.default = roles[r][1]
+			db.session.add(role)
+		db.session.commit()
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
 
 
 class User(UserMixin, db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(64), unique=True, index=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    password_hash = db.Column(db.String(128))
-    confirmed = db.Column(db.Boolean, default=False)
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String(64), unique=True, index=True)
+	username = db.Column(db.String(64), unique=True, index=True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+	password_hash = db.Column(db.String(128))
+	confirmed = db.Column(db.Boolean, default=False)
 
-    @property
-    def password(self):
-        raise AttributeError('password is not a readable attribute')
+	@property
+	def password(self):
+		raise AttributeError('password is not a readable attribute')
 
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
 
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
 
-    def generate_confirmation_token(self, expiration=3600):
-        s = Serializer(current_app.config['SECRET_KEY'], expiration)
-        return s.dumps({'confirm': self.id})
+	def generate_confirmation_token(self, expiration=3600):
+		s = Serializer(current_app.config['SECRET_KEY'], expiration)
+		return s.dumps({'confirm': self.id})
 
-    def confirm(self, token):
-        s = Serializer(current_app.config['SECRET_KEY'])
-        try:
-            data = s.loads(token)
-        except:
-            return False
-        if data.get('confirm') != self.id:
-            return False
-        self.confirmed = True
-        db.session.add(self)
-        return True
+	def confirm(self, token):
+		s = Serializer(current_app.config['SECRET_KEY'])
+		try:
+			data = s.loads(token)
+		except:
+			return False
+		if data.get('confirm') != self.id:
+			return False
+		self.confirmed = True
+		db.session.add(self)
+		return True
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+	def __repr__(self):
+		return '<User %r>' % self.username
 
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+	return User.query.get(int(user_id))
