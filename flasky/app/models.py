79e8f7076d04ec1dd0a2f8e84c5e899162538834
lizhengxiang <1065482100@qq.com>
2015-05-19 16:13:44 +0800

    user message

--- flasky/app/models.py
+++ flasky/app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
@@ -53,6 +54,11 @@ class User(UserMixin, db.Model):
 	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 	password_hash = db.Column(db.String(128))
 	confirmed = db.Column(db.Boolean, default=False)
+	name = db.Column(db.String(64))
+	location = db.Column(db.String(64))
+	about_me = db.Column(db.Text())
+	number_since = db.Column(db.DateTime(), default = datetime.utcnow)
+	last_seen = db.Column(db.DateTime(), default = datetime.utcnow)
 
 	@property
 	def password(self):
