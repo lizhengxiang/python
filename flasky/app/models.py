e280d3259fd4ca337a996ab214056491a87e97e0
lizhengxiang <1065482100@qq.com>
2015-05-19 14:25:12 +0800

    debug KeyError: 'FLASKY_ADMI'

--- flasky/app/models.py
+++ flasky/app/models.py
@@ -82,7 +82,7 @@ class User(UserMixin, db.Model):
 		return True
 	def __init__(self, **kwargs):
 		super(User, self).__init__(**kwargs)
-		if self.email == current_app.config['FLASKY_ADMI']:
+		if self.email == current_app.config['FLASKY_ADMIN']:
 			self.role = Role.query.filter_by(permissions = 0xff).first()
 		if self.role is None:
 			self.role = Role.query.filter_by(default = True).first()
