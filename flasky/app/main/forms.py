44981629758ee3ee9bc14b618377ae54901dfb17
lizhengxiang <1065482100@qq.com>
2015-05-21 00:46:23 +0800

    EditprofileAdminForm

--- flasky/app/main/forms.py
+++ flasky/app/main/forms.py
@@ -1,5 +1,5 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, BooleanFiled
 from wtforms.validators import Required, Length, Email, Regexp
 #from wtforms import ValidationError
 #from flask.ext.pagedown.fields import PageDownField
@@ -14,5 +14,30 @@ class EditprofileForm(Form):
 	name = StringField('Real name', validators = [Length(0, 64)])
 	location = StringField('Location', validators=[Length(0, 64)])
 	about_me = TextAreaField('About me')
-	submit = SubmitField('Submet')
+	submit = SubmitField('Submit')
 
+class EditprofileAdminFrom(Form):
+	email = StringField('Email', validators = [Required(), Length(0, 64), Email()]
+	username = StringField('Username', validators = [Required(), Length(0, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 
+			'Username must have only,number, dots or unedrscores')])
+	confirmed = BooleanFiled('Confirmed')
+	role = selectField('Role', coerce = int)
+	name = StringField('Role Name', validators = [Length(0, 64)])	
+	location = StringField('Location', validators = [Length(0, 64)])
+	about_me = TextAreaField('About me')
+	submit = SubmitField('Submit')
+	
+	def __init__(self, user, *args, **kwargs):
+		super(EditprofileAdminFrom, self).__init__(*args, **kwargs):
+			self.role.choices = [(role.id, role.name)
+								for role in Role.order_by(Role.name).all()]
+			self.user = user
+	def validate_email(self, field):
+		if field.data != self.user.email and \
+				User.query.filter_by(email = field.data).first():
+			raise ValidationError('Email already register.')
+	
+	def validate_username(self, field):
+		if field.data != self.user.username and \
+				User.query.filter_by(username = field.data).first():
+			raise ValidationError('Username already register.')
