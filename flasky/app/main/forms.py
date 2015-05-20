b46d1da8a30cef95c84f584f75c32af1a608d6ee
lizhengxiang <1065482100@qq.com>
2015-05-20 16:19:34 +0800

    user massage

--- flasky/app/main/forms.py
+++ flasky/app/main/forms.py
@@ -9,3 +9,10 @@ from wtforms.validators import Required, Length, Email, Regexp
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+
+class EditprofileForm(From):
+	name = StringField('Real name', validators = [Length(0, 64)])
+	location = StringField('Location', validators=[Length(0, 64)])
+	about_me = TextAreaFiled('About me')
+	submit = SubmitFiled('Submet')
+
